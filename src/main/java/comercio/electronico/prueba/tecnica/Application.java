a123143decd6792188ce8eeb3cd0ecaf6237074e
msulccha <mau.sulca@gmail.com>
2023-05-20 23:53:33 -0500

    chor: prueba tecnica

---
+++ src/main/java/comercio/electronico/prueba/tecnica/Application.java
@@ -0,0 +1,13 @@
+package comercio.electronico.prueba.tecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/comercio/electronico/prueba/tecnica/ApplicationTests.java
@@ -0,0 +1,13 @@
+package comercio.electronico.prueba.tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
